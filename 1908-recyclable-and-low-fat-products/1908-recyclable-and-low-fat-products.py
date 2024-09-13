76e28b9075f11c9eb84277a40badb0499dd0279e
young2good <ufo2989@gmail.com>
2024-09-14 07:14:57 +0900

    Time: 391 ms (54.99%), Space: 70.4 MB (13.84%) - LeetHub

--- 1908-recyclable-and-low-fat-products/1908-recyclable-and-low-fat-products.py
+++ 1908-recyclable-and-low-fat-products/1908-recyclable-and-low-fat-products.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 def find_products(products: pd.DataFrame) -> pd.DataFrame:
-    df_result = products[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')]
-    df_result = df_result[['product_id']]
+    df_filtered = products[(products['low_fats']=='Y')&(products['recyclable']=='Y')]
+
+    df_result = df_filtered[['product_id']]
 
     return df_result
