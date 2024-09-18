e40a6c35520fd937edff1d0525439605d4507b8d
young2good <ufo2989@gmail.com>
2024-09-18 12:21:17 +0900

    Time: 342 ms (76.73%), Space: 70.4 MB (23.47%) - LeetHub

---
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def delete_duplicate_emails(person: pd.DataFrame) -> None:
+    person.sort_values('id', inplace = True)
+    person.drop_duplicates(subset = 'email', inplace = True)
+    person.reset_index(inplace = True, drop = True)
+
