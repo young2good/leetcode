2fb449e7615e64c5f00643963ecd7bbabe07520e
young2good <ufo2989@gmail.com>
2024-09-21 04:18:30 +0900

    Time: 451 ms (39.82%), Space: 71.7 MB (51.37%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.py
+++ 0184-department-highest-salary/0184-department-highest-salary.py
@@ -1,12 +1,13 @@
 import pandas as pd
 
 def department_highest_salary(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
-    df_joined = pd.merge(employee, department, how ='left', left_on = 'departmentId', right_on = 'id')
-    max_sal = df_joined.groupby('name_y')['salary'].max().reset_index()
+    df_ranked = employee
+    df_ranked['rank'] = df_ranked.groupby('departmentId')['salary'].rank(method = 'dense', ascending = False)
 
-    new_df_joined = pd.merge(df_joined, max_sal,how='left', on = 'name_y')
-    res_df = new_df_joined[new_df_joined['salary_x'] ==new_df_joined['salary_y']][['name_y','name_x','salary_y']]
-    res_df.columns = ['Department', 'Employee', 'Salary']
+    df_filtered = df_ranked[df_ranked['rank']==1]
 
-    return res_df
+    df_merged = pd.merge(df_filtered, department, how = 'left', left_on = 'departmentId', right_on = 'id')
     
+    df_result = df_merged[['name_y', 'name_x', 'salary']]
+    df_result.columns = ['Department', 'Employee', 'Salary']
+    return df_result
