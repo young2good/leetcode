bac245584f42720b31a87b1a9e1476ac4adbfcbe
young2good <ufo2989@gmail.com>
2024-09-21 04:25:06 +0900

    Time: 652 ms (49.61%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,28 +1,20 @@
-/* Write your T-SQL query statement below */
-
-with employee_salary as (
-        Select T1.*
-            , T2.name as department_name
-            from Employee T1
-            LEFT JOIN Department T2
-            ON T1.departmentId = T2.id
-)
-
-select T2.department_name as Department
-     , T2.name as Employee
-     , T2. salary as Salary
-    from
-    (
-        select department_name 
-            , max(Salary) as max_salary
-            FROM employee_salary
-            GROUP BY department_name
-    ) T1
-    LEFT JOIN
+# Write your MySQL query statement below
+SELECT Department
+     , name as Employee
+     , salary as Salary
+    FROM
     (
         SELECT *
-            from employee_salary
-    ) T2
-    ON T1.department_name = T2.department_name
-    AND T1.max_salary = T2.Salary
+            , dense_rank() over (partition by Department ORDER BY salary desc) as dr
+            FROM
+            (
+                SELECT T1.*
+                    , T2.name as Department
+                    FROM Employee T1
+                    LEFT JOIN Department T2
+                    ON T1.departmentId = T2.id
+            ) T
+    ) K
+    WHERE 1=1 
+      AND dr = 1
 ;
