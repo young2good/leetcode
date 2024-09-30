ddbe7e438992591c13b691cae9f2053c02ddc277
young2good <ufo2989@gmail.com>
2024-09-30 13:34:00 +0900

    Time: 373 ms (64.89%), Space: 71.3 MB (60.34%) - LeetHub

--- 1161-project-employees-i/1161-project-employees-i.py
+++ 1161-project-employees-i/1161-project-employees-i.py
@@ -1,13 +1,9 @@
 import pandas as pd
 
 def project_employees_i(project: pd.DataFrame, employee: pd.DataFrame) -> pd.DataFrame:
-    df_joined = pd.merge(project, employee, how = 'left', on = 'employee_id')
+    df_joined = pd.merge(project, employee[['employee_id', 'experience_years']], how = 'left', on = 'employee_id')
 
-    df_grouped = df_joined.groupby('project_id')['experience_years'].mean()
-    df_grouped = df_grouped.reset_index()
-
-    df_grouped['experience_years'] = df_grouped['experience_years'].round(2)
-
+    df_grouped = df_joined.groupby('project_id', as_index = False)['experience_years'].mean().round(2)
     df_grouped = df_grouped.rename(columns = {'experience_years':'average_years'})
 
     return df_grouped
