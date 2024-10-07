7bb54345ed3d2c8aebb5e390da52965a820eda30
young2good <ufo2989@gmail.com>
2024-10-07 15:39:42 +0900

    Time: 1948 ms (11.91%), Space: 0B (100%) - LeetHub

--- 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
@@ -1,14 +1,11 @@
-# Write your MySQL query statement below
-
-with tot_users AS (
-    SELECT COUNT(distinct user_id) as users
-        FROM Users
-) 
-
-SELECT contest_id
-     , round(count(distinct user_id) / min(users) * 100, 2) as percentage
-    FROM Register T1
-    JOIN tot_users T2
-    GROUP BY contest_id
-    ORDER BY 2 desc, 1 asc
-    ;
+-- Register 테이블에서 퍼센트를 계산하는 서브쿼리 방식 사용
+SELECT 
+    contest_id, 
+    ROUND(COUNT(DISTINCT user_id) / (SELECT COUNT(DISTINCT user_id) FROM Users) * 100, 2) AS percentage
+FROM 
+    Register
+GROUP BY 
+    contest_id
+ORDER BY 
+    percentage DESC, 
+    contest_id ASC;
