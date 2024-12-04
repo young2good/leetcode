7fea19ba40dd311a4ece5706f2cda1e65b5d85f2
young2good <ufo2989@gmail.com>
2024-12-04 10:56:07 +0900

    Time: 552 ms (84.37%), Space: 0B (100%) - LeetHub

--- 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
@@ -1,6 +1,7 @@
-/* Write your T-SQL query statement below */
+# Write your MySQL query statement below
 
-select customer_id from Customer
-group by customer_id
-HAVING count(distinct product_key) = (select count(distinct product_key) from Product)
-;
+SELECT customer_id
+    FROM Customer
+    GROUP BY customer_id
+    HAVING count(distinct product_key) = (select count(distinct product_key) from Product)
+    ; 
