098ae62a27a9ec161d83359570295cfb487fb716
young2good <ufo2989@gmail.com>
2025-01-03 11:38:41 +0900

    Time: 1687 ms (34.04%), Space: 0B (100%) - LeetHub

--- 1327-last-person-to-fit-in-the-bus/1327-last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/1327-last-person-to-fit-in-the-bus.sql
@@ -1,12 +1,16 @@
 # Write your MySQL query statement below
+WITH last_turn AS (
+        SELECT max(turn) last_turn
+            FROM
+            (
+                SELECT *
+                    , SUM(weight) OVER(ORDER BY turn) as agg_sum
+                    FROM Queue
+            ) T
+            WHERE agg_sum<=1000
+)
+
 SELECT person_name
-    FROM
-    (
-        SELECT *
-            , SUM(weight) OVER(ORDER BY turn) as agg_sum
-            FROM Queue
-    ) T
-    WHERE agg_sum<=1000
-    ORDER BY turn DESC
-    LIMIT 1
+    FROM Queue
+    WHERE turn in (select * from last_turn)
     ;
