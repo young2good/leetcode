e37b3bcf373c10fedfc3dbd71eee9950ea4bb7e9
young2good <ufo2989@gmail.com>
2025-01-03 12:30:47 +0900

    Time: 617 ms (19.33%), Space: 0B (100%) - LeetHub

--- 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
@@ -4,8 +4,7 @@ SELECT employee_id
     WHERE manager_id in (
                             SELECT distinct manager_id
                                 FROM Employees
-                                WHERE salary < 30000
-                                AND manager_id not in (select employee_id from Employees)
+                                WHERE manager_id not in (select employee_id from Employees)
     ) 
       AND salary < 30000
     ORDER BY employee_id
