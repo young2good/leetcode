f98c1a75c2b3e6342e74ed75429da6e3c7ff6aa9
young2good <ufo2989@gmail.com>
2025-03-21 08:56:27 +0900

    Time: 1373 ms (95.02%), Space: 0B (100%) - LeetHub

--- 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
@@ -1,26 +1,19 @@
 # Write your MySQL query statement below
+WITH second_date AS (
+    SELECT *
+         , DATE_ADD(first_date, interval 1 DAY) as second_date
+         FROM
+         (
+            SELECT player_id
+                , min(event_date) as first_date
+                FROM Activity
+                GROUP BY player_id
+         ) T
+)
 
-SELECT round(sum(case when next_day is not null then 1 else 0 end)/ count(distinct T.player_id),2) fraction
-    FROM
--- SELECT T.*
---      , T2.next_day
---      FROM
-    (
-        SELECT *
-            FROM Activity
-    ) T
-    LEFT JOIN
-    (
-        SELECT *
-             , date_add(event_date, INTERVAL 1 DAY) as next_day
-            FROM Activity
-            WHERE (player_id, event_date) in (
-                SELECT player_id
-                     , min(event_date) as first_date
-                     FROM Activity
-                     GROUP BY player_id
-            )
-    ) T2
-    ON T.event_date = T2.next_day
-    AND T.player_id = T2.player_id
-    
+SELECT convert(count(distinct T2.player_id) / count(distinct T1.player_id), decimal(3,2)) fraction
+    FROM Activity T1
+    LEFT JOIN second_date T2
+    ON T1.player_id = T2.player_id
+    AND T1.event_date = T2.second_date
+    ;
