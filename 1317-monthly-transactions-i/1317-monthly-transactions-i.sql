5e5b7e9c471339dd6cecccb0dd90a9ce998d805c
young2good <ufo2989@gmail.com>
2025-03-20 22:19:22 +0900

    Time: 1055 ms (57.76%), Space: 0B (100%) - LeetHub

--- 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
@@ -1,13 +1,10 @@
 # Write your MySQL query statement below
-
--- SELECT concat(YEAR(trans_date), '-', case when MONTH(trans_date) < 10 then concat(0,MONTH(trans_date))else MONTH(trans_date) end) 'month'
-SELECT date_format(trans_date, '%Y-%m') 'month'
--- SELECT concat(YEAR(trans_date), '-', LPAD(MONTH(trans_date), 2, 0)) 'month'
+SELECT left(trans_date, 7) as month
      , country
-     , count(*) trans_count
-     , sum(case when state = 'approved' then 1 else 0 end) approved_count
-     , sum(amount) trans_total_amount
-     , sum(case when state = 'approved' then amount else 0 end) approved_total_amount
+     , count(*) as trans_count
+     , sum(case when state = 'approved' then 1 else 0 end) as approved_count
+     , sum(amount) as trans_total_amount
+     , ifnull(sum(case when state = 'approved' then amount end), 0) as approved_total_amount
     FROM Transactions
-    GROUP BY date_format(trans_date, '%Y-%m'), country
+    GROUP BY left(trans_date, 7), country
     ;
