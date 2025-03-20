ae204e5b7a286fca8e1fde474beb78035628b571
young2good <ufo2989@gmail.com>
2025-03-20 21:44:02 +0900

    Time: 893 ms (84.96%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -1,19 +1,16 @@
 # Write your MySQL query statement below
+WITH CALCULATED_CONFIRMATION AS (
+    SELECT user_id
+        , COUNT(user_id) as total_number
+        , SUM(CASE WHEN action = 'confirmed' THEN 1 ELSE 0 END) AS confirmed_count
+        , SUM(CASE WHEN action = 'confirmed' THEN 1 ELSE 0 END) / COUNT(user_id) as confirmation_rate
+        FROM Confirmations
+        GROUP BY user_id
+)
+
 SELECT T1.user_id
-     , round(IFNULL(T2.confirmation_rate,0),2) as confirmation_rate
-    FROM
-    (
-        SELECT *
-            FROM Signups
-    ) T1
-    LEFT JOIN
-    (
-        SELECT user_id
-            , sum(CASE WHEN action = 'confirmed' then 1 else 0 end) / count(*) as confirmation_rate
-            FROM Confirmations
-            WHERE 1=1
-            GROUP BY user_id
-    ) T2
+     , CONVERT(IFNULL(confirmation_rate, 0), decimal(3,2)) as confirmation_rate
+    FROM Signups T1
+    LEFT JOIN CALCULATED_CONFIRMATION T2
     ON T1.user_id = T2.user_id
-    
-;
+    ;
