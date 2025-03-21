5d53faa2974201e4e92ff2754472401ae29bfb46
young2good <ufo2989@gmail.com>
2025-03-21 09:25:51 +0900

    Time: 928 ms (95.24%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,24 +1,9 @@
 # Write your MySQL query statement below
-WITH RECURSIVE RE_QUERY AS (
-    SELECT 1 as num
-    UNION ALL
-    SELECT num + 1 as num
-        FROM RE_QUERY
-        WHERE num < 10
-)
 
-SELECT distinct salary AS SecondHighestSalary
+SELECT MAX(CASE WHEN DR = 2 THEN salary ELSE NULL END) SecondHighestSalary
     FROM
     (
         SELECT *
-            FROM RE_QUERY
-    ) T1
-    LEFT JOIN
-    (
-        SELECT *
-            , DENSE_RANK() OVER(ORDER BY salary DESC) as DR
+            , DENSE_RANK() OVER(ORDER BY salary DESC) DR
             FROM Employee
-    ) T2
-    ON T1.num = T2.DR
-    WHERE num=2
-    ;
+    ) T
