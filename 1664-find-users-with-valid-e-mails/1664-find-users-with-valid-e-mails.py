383ca9d7f7cb6af8d1d087b4c79602400ea09624
young2good <ufo2989@gmail.com>
2025-04-17 22:44:55 +0900

    Time: 331 ms (60.19%), Space: 68.2 MB (7.82%) - LeetHub

--- 1664-find-users-with-valid-e-mails/1664-find-users-with-valid-e-mails.py
+++ 1664-find-users-with-valid-e-mails/1664-find-users-with-valid-e-mails.py
@@ -6,15 +6,14 @@ def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
     df_filtered['domain'] = users['mail'].str.split('@').str[1]
 
     df_filtered['is_valid'] = df_filtered['prefix'].apply(
-        lambda x: True if all([
+        lambda x: all([
             i.isalnum() or i =='_' or i == '-' or i =='.'
             for i in x
-            ]) else False
-            )
+            ]) and x[0].isalpha()
+    )
 
     df_result = df_filtered[
         (df_filtered['domain'] == 'leetcode.com') &
-        (df_filtered['is_valid'] == True) &
-        (df_filtered['prefix'].str[0].str.isalpha())
+        (df_filtered['is_valid'])
         ]
     return df_result[['user_id','name','mail']]
