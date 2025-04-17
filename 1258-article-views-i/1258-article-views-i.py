18ff9068df692398647875b0151da2273247dde8
young2good <ufo2989@gmail.com>
2025-04-17 16:53:11 +0900

    Time: 337 ms (56.96%), Space: 67.2 MB (34.57%) - LeetHub

--- 1258-article-views-i/1258-article-views-i.py
+++ 1258-article-views-i/1258-article-views-i.py
@@ -3,9 +3,10 @@ import pandas as pd
 def article_views(views: pd.DataFrame) -> pd.DataFrame:
     df_filtered = views[views['author_id'] == views['viewer_id']]
 
-    df_result = pd.DataFrame(df_filtered['author_id'].unique())
+    df_filtered = df_filtered[['author_id']]
+
+    df_result = pd.DataFrame(df_filtered[~df_filtered.duplicated()])
     df_result.columns = ['id']
-    df_result = df_result.sort_values(by = 'id')
-    
+    df_result = df_result.sort_values('id')
     return df_result
     
