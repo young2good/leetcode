8faea6e76e17495c139eb66f3bc0960725a509ac
young2good <ufo2989@gmail.com>
2025-04-17 14:10:14 +0900

    Time: 296 ms (71.39%), Space: 67.5 MB (33.9%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.py
+++ 0183-customers-who-never-order/0183-customers-who-never-order.py
@@ -1,13 +1,8 @@
 import pandas as pd
 
 def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
-    df_order_cnt = orders.groupby('customerId')['id'].count().reset_index()
-    df_order_cnt.columns =['customerId','order_cnt']
+    order_customer = orders['customerId'].unique()
 
-    df_is_order = customers.merge(df_order_cnt, how = 'left', left_on = 'id', right_on = 'customerId').fillna(0)
-    
-    df_result = df_is_order[df_is_order['order_cnt']==0]
-    df_result = df_result[['name']]
-
-    df_result.columns = ['Customers']
+    df_result = customers[~customers['id'].isin(order_customer)]
+    df_result = df_result[['name']].rename(columns ={'name':'Customers'})
     return df_result
