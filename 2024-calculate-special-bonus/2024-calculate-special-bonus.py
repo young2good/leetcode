9f0a5725dfaccb37df7b56ef494d7f9dd700e4b8
young2good <ufo2989@gmail.com>
2025-04-17 17:08:31 +0900

    Time: 342 ms (66.33%), Space: 67.8 MB (26.77%) - LeetHub

--- 2024-calculate-special-bonus/2024-calculate-special-bonus.py
+++ 2024-calculate-special-bonus/2024-calculate-special-bonus.py
@@ -1,25 +1,8 @@
 import pandas as pd
-import numpy as np
-
-# def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
-#     df_employees = employees
-    
-#     df_employees['is_odd'] = df_employees['employee_id'].apply(lambda x:True if x%2 != 0 else False)
-#     df_employees['start_with_m'] = df_employees['name'].apply(lambda x:True if x[0] == 'M' else False)
-#     # df_employees['bonus'] = df_employees.apply(lambda x:x['salary'] if (x['is_odd']==True) & (x['start_with_m']==False) else 0, axis = 1)
-#     df_employees['bonus'] = np.where((df_employees['is_odd'] == True) & (df_employees['start_with_m']==False), df_employees['salary'],0)
-
-#     df_result = df_employees[['employee_id','bonus']]
-#     df_result = df_result.sort_values('employee_id')
-
-#     return df_result
 
 def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
-    df_employees = employees
-    # df_employees['start_with_m'] = df_employees['name'].apply(lambda x:True if x[0] == 'M' else False)
-    df_employees['bonus'] = np.where((df_employees['employee_id']%2 == 1) & (~df_employees['name'].str.startswith('M')), df_employees['salary'], 0)
-
-    df_result = df_employees[['employee_id', 'bonus']]
-    df_result = df_result.sort_values('employee_id')
-
-    return df_result
+    bonus_employees = employees
+    bonus_employees['first_character'] = bonus_employees['name'].apply(lambda x:x[0])
+    bonus_employees['is_even'] = bonus_employees['employee_id'].apply(lambda x:mod(x,2))
+    bonus_employees['bonus'] = bonus_employees.apply(lambda x:x.salary if (x.first_character != 'M') & (x.is_even == 1) else 0, axis = 1)
+    return bonus_employees[['employee_id', 'bonus']].sort_values('employee_id')
