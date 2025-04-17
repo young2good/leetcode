88cf9aaaf7dfd18993f10dcf02e1d0aa67563ec4
young2good <ufo2989@gmail.com>
2025-04-17 20:04:40 +0900

    Time: 317 ms (79.98%), Space: 68.1 MB (9.37%) - LeetHub

--- 2024-calculate-special-bonus/2024-calculate-special-bonus.py
+++ 2024-calculate-special-bonus/2024-calculate-special-bonus.py
@@ -1,8 +1,7 @@
 import pandas as pd
 
 def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
-    bonus_employees = employees
-    bonus_employees['first_character'] = bonus_employees['name'].apply(lambda x:x[0])
-    bonus_employees['is_even'] = bonus_employees['employee_id'].apply(lambda x:mod(x,2))
-    bonus_employees['bonus'] = bonus_employees.apply(lambda x:x.salary if (x.first_character != 'M') & (x.is_even == 1) else 0, axis = 1)
-    return bonus_employees[['employee_id', 'bonus']].sort_values('employee_id')
+    employees['bonus'] = employees.loc[(employees['employee_id'] % 2 == 1) & (~employees['name'].str.startswith('M')), 'salary']
+    employees['bonus'] = employees['bonus'].fillna(0)
+    
+    return employees[['employee_id','bonus']].sort_values('employee_id')
