d5acc0289f17c9094cb3d179ea84d184998366c6
young2good <ufo2989@gmail.com>
2025-04-17 16:44:57 +0900

    Time: 299 ms (71.93%), Space: 67.5 MB (10.62%) - LeetHub

--- 1258-article-views-i/1258-article-views-i.py
+++ 1258-article-views-i/1258-article-views-i.py
@@ -5,5 +5,7 @@ def article_views(views: pd.DataFrame) -> pd.DataFrame:
 
     df_result = pd.DataFrame(df_filtered['author_id'].unique())
     df_result.columns = ['id']
-    df_result = df_result.sort_values('id', ascending =True)
+    df_result = df_result.sort_values(by = 'id')
+    
     return df_result
+    
