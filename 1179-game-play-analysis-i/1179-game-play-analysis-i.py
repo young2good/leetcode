ad5058beb466f9dcc2ecaf27e0df5b1594c5830b
young2good <ufo2989@gmail.com>
2025-05-09 07:36:42 +0900

    Time: 279 ms (87.52%), Space: 68 MB (57.12%) - LeetHub

--- 1179-game-play-analysis-i/1179-game-play-analysis-i.py
+++ 1179-game-play-analysis-i/1179-game-play-analysis-i.py
@@ -1,8 +1,11 @@
 import pandas as pd
 
 def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
-    df_grouped = activity.groupby('player_id', as_index = False)['event_date'].min()
-    # df_grouped = df_grouped.reset_index()
+    df = activity.copy()
 
-    df_grouped  = df_grouped.rename(columns = {'event_date':'first_login'})
-    return df_grouped
+    df.rename(columns ={'event_date':'first_login'}, inplace = True)
+
+    df_groupped = df.groupby('player_id')['first_login'].min().reset_index()
+
+
+    return df_groupped
