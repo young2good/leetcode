18d6a431de4d1e414e5e7da65d203b348316fd52
young2good <ufo2989@gmail.com>
2025-05-09 23:37:02 +0900

    Time: 275 ms (56.58%), Space: 69.2 MB (17.24%) - LeetHub

--- 0595-big-countries/0595-big-countries.py
+++ 0595-big-countries/0595-big-countries.py
@@ -1,11 +1,4 @@
 import pandas as pd
 
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
-    df_world = world
-    df_filtered = df_world[
-        (df_world['area'] >= 3000000) |
-        (df_world['population']>= 25000000)
-    ]
-
-    df_result = df_filtered[['name','population','area']]
-    return df_result
+    return world[(world['area']>=3000000) | (world['population']>=25000000)][['name','population','area']]
