3f370368ebd587b11b34f56eee1fe6f7c235ccb2
young2good <ufo2989@gmail.com>
2025-05-10 15:54:40 +0900

    Time: 305 ms (50.76%), Space: 67.5 MB (36.71%) - LeetHub

--- 1827-invalid-tweets/1827-invalid-tweets.py
+++ 1827-invalid-tweets/1827-invalid-tweets.py
@@ -1,9 +1,6 @@
 import pandas as pd
 
 def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
-    df_tweets = tweets
+    tweets['con_len'] = tweets['content'].apply(lambda x:len(x))
 
-    df_tweets['con_len'] = df_tweets.apply(lambda x: len(x.content), axis =1)
-    df_result = df_tweets[df_tweets['con_len']>15]
-
-    return df_result[['tweet_id']]
+    return tweets[tweets['con_len'] > 15][['tweet_id']]
