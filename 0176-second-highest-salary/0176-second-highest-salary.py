4c86bc6f860e9d02f41cc080a96ce6759902d55f
young2good <ufo2989@gmail.com>
2025-05-11 18:03:14 +0900

    Time: 263 ms (64.59%), Space: 68 MB (8.91%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.py
+++ 0176-second-highest-salary/0176-second-highest-salary.py
@@ -1,10 +1,21 @@
 import pandas as pd
 
 def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
-    employee['sal_rank'] = employee['salary'].rank(ascending = False, method = 'dense')
+    df_employee = employee.copy()
 
-    employee['output_result'] = employee.apply(lambda x:x['salary'] if x['sal_rank'] ==2 else None, axis = 1)
+    # df_employee['rank'] = df_employee['salary'].rank(ascending = False, method = 'dense')
 
-    df_result = pd.DataFrame(employee[['output_result']].max(), columns = ['SecondHighestSalary'])
+    unique_salary = df_employee['salary'].unique()
+
+    df_new = pd.DataFrame(unique_salary, columns=['salary'])
+
+    df_new['rank'] = df_new['salary'].rank(ascending = False)
+
+    if len(unique_salary) < 2:
+        df_result = pd.DataFrame({'SecondHighestSalary': [None]})
+    else:
+        df_result = df_new[df_new['rank']==2][['salary']]
+
+    df_result.columns = ['SecondHighestSalary']
 
     return df_result
