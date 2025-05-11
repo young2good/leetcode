2ff08309eb7432da117ddebb6cfad051cfe534b4
young2good <ufo2989@gmail.com>
2025-05-11 19:01:49 +0900

    Time: 272 ms (51.76%), Space: 67.3 MB (47.51%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.py
+++ 0176-second-highest-salary/0176-second-highest-salary.py
@@ -1,21 +1,28 @@
 import pandas as pd
 
 def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
+    # df_employee = employee.copy()
+
+    # unique_salary = df_employee['salary'].unique()
+
+    # df_new = pd.DataFrame(unique_salary, columns=['salary'])
+
+    # df_new['rank'] = df_new['salary'].rank(ascending = False)
+
+    # if len(unique_salary) < 2:
+    #     df_result = pd.DataFrame({'SecondHighestSalary': [None]})
+    # else:
+    #     df_result = df_new[df_new['rank']==2][['salary']]
+    # df_result.columns = ['SecondHighestSalary']
+    # return df_result
+
     df_employee = employee.copy()
 
-    # df_employee['rank'] = df_employee['salary'].rank(ascending = False, method = 'dense')
-
-    unique_salary = df_employee['salary'].unique()
-
-    df_new = pd.DataFrame(unique_salary, columns=['salary'])
-
-    df_new['rank'] = df_new['salary'].rank(ascending = False)
+    unique_salary = df_employee['salary'].sort_values(ascending = False).unique()
 
     if len(unique_salary) < 2:
-        df_result = pd.DataFrame({'SecondHighestSalary': [None]})
+        result = None
     else:
-        df_result = df_new[df_new['rank']==2][['salary']]
+        result = unique_salary[1]
 
-    df_result.columns = ['SecondHighestSalary']
-
-    return df_result
+    return pd.DataFrame({"SecondHighestSalary": [result]})
