aa7be279cda0a78934da67336ed4a811f0fe1d33
Younghyun Kim <ufo2989@gmail.com>
2025-06-13 16:29:36 +0900

    Time: 578 ms (95.11%), Space: 0B (100%) - LeetHub

--- 1811-fix-names-in-a-table/1811-fix-names-in-a-table.sql
+++ 1811-fix-names-in-a-table/1811-fix-names-in-a-table.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
+
 SELECT user_id
-     , concat(upper(left(name,1)), lower(substring(name from 2))) as name
+     , CONCAT(UPPER(left(name,1)), LOWER(mid(name,2))) as name
     FROM Users
-    order by user_id
+    ORDER BY user_id
